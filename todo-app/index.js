5d186f89920b1c0d00ea484b4b1438ad6da0d930
Roy <taeheeusc@gmail.com>
2026-08-29 19:03:21 +0000

    Serve an html page from the todo app

--- todo-app/index.js
+++ todo-app/index.js
@@ -2,9 +2,22 @@ const http = require('http')
 
 const PORT = process.env.PORT || 3000
 
+const page = `<!DOCTYPE html>
+<html>
+  <head><title>Todo app</title></head>
+  <body>
+    <h1>Todo app</h1>
+    <ul>
+      <li>Learn Kubernetes</li>
+      <li>Write manifests</li>
+    </ul>
+  </body>
+</html>
+`
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Todo app\n')
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.end(page)
 })
 
 server.listen(PORT, () => {
