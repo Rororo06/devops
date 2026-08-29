a104a86137628b76d19562a51ce71c4646d23789
Roy <taeheeusc@gmail.com>
2026-08-29 19:04:07 +0000

    Serve log output status over http behind an ingress

--- log_output/index.js
+++ log_output/index.js
@@ -1,10 +1,24 @@
+const http = require('http')
 const { randomUUID } = require('crypto')
 
+const PORT = process.env.PORT || 3000
 const id = randomUUID()
 
+let timestamp = new Date().toISOString()
+
 const output = () => {
-  console.log(`${new Date().toISOString()}: ${id}`)
+  timestamp = new Date().toISOString()
+  console.log(`${timestamp}: ${id}`)
 }
 
 output()
 setInterval(output, 5000)
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end(`${timestamp}: ${id}\n`)
+})
+
+server.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`)
+})
